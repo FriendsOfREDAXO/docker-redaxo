799fd8cf3eab8eb634f37554974fc006e96efc08
schuer <ds@decaf.de>
2023-12-09 14:02:48 +0100

    build: add scripts

---
+++ scripts/generate-builds.ts
@@ -0,0 +1,70 @@
+import { isArray, isObject } from "std/yaml/_utils.ts";
+import { parse } from "std/yaml/parse.ts";
+import { emptyDirSync } from "std/fs/empty_dir.ts";
+import { ensureDirSync } from "std/fs/ensure_dir.ts";
+import { copySync } from "std/fs/copy.ts";
+
+const sourceDirectory = 'source';
+const buildsDirectory = 'builds';
+
+/**
+ * Prepare build configuration
+ */
+const buildConfiguration = parse(Deno.readTextFileSync(`${sourceDirectory}/builds.yml`));
+
+if (!isObject(buildConfiguration)) {
+	console.error("Invalid build configuration!");
+	Deno.exit(1);
+}
+
+const builds = buildConfiguration["builds"];
+
+if (!isArray(builds)) {
+	console.error("Invalid builds array!");
+	Deno.exit(1);
+}
+
+/**
+ * Clear builds directory
+ * Hint: we want all builds to be removed that are no longer contained in the build configuration
+ */
+emptyDirSync(buildsDirectory);
+
+/**
+ * Generate builds
+ */
+const dockerfileSource = Deno.readTextFileSync(`${sourceDirectory}/Dockerfile`);
+
+for (const build of builds) {
+
+	const targetDir = `${buildsDirectory}/${build["name"]}`;
+	ensureDirSync(targetDir);
+
+	/**
+	 * Generate Dockerfile
+	 */
+
+	// handle placeholders
+	const replacements: Record<string, string> = {
+		'%%PHP_VERSION_TAG%%': `${build["php-version"]}`,
+		'%%PACKAGE_URL%%': `${build["package-url"]}`,
+		'%%PACKAGE_SHA%%': `${build["package-sha"]}`,
+	};
+	let currentDockerfileSource = dockerfileSource;
+	Object.keys(replacements).forEach((key) => {
+		currentDockerfileSource = currentDockerfileSource.replaceAll(key, replacements[key]);
+	});
+
+	Deno.writeTextFileSync(`${targetDir}/Dockerfile`, currentDockerfileSource);
+
+	/**
+	 * Copy static files that do not require replacements
+	 */
+	const filesToCopy = [
+		`docker-entrypoint.sh`,
+		`README.md`,
+	];
+	filesToCopy.forEach(file => {
+		copySync(`${sourceDirectory}/${file}`, `${targetDir}/${file}`);
+	})
+}
